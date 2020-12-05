fd6830b7ec5924de9569410db17eaf0ebb62d303
cheap-glitch <cheap.glitch@gmail.com>
2020-12-05 16:35:56 +0100

    🔨 build: Move build artefacts to `build` folder

--- utils/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.secret = void 0;
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-exports.secret = process.env.FAUNADB_SECRET;
--- utils/utils.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.wait = void 0;
-async function wait(delay) {
-    return new Promise(resolve => setTimeout(resolve, delay));
-}
-exports.wait = wait;
