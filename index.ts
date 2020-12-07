90ba828f1d092476b78f010f484b60cd16c6d18c
cheap-glitch <cheap.glitch@gmail.com>
2020-12-07 13:04:04 +0100

    🔨 build: Bundle build files

--- index.ts
+++ index.ts
@@ -7,9 +7,9 @@
  * This software is distributed under the ISC license
  */
 
-import { getFaunaClient } from './utils/client';
-import { uploadSchema } from './lib/schema';
-import { uploadResources } from './lib/resources';
+import { getFaunaClient } from './src/utils/client';
+import { uploadSchema } from './src/lib/schema';
+import { uploadResources } from './src/lib/resources';
 
 console.info(getFaunaClient);
 console.info(uploadSchema);
