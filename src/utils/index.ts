ffeb706bdbc0eecb8ebdfdbdfc107d65042459c8
cheap-glitch <cheap.glitch@gmail.com>
2020-12-11 12:47:14 +0100

    🚩 test: Update tests

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,18 @@
+// import { errors } from 'faunadb/src/types/errors';
+
+/*
+export function prettifyFaunaError(error: errors.FaunaHTTPError): string {
+	if (error.requestResult && error.requestResult.requestRaw) {
+		error.requestResult.requestRaw = JSON.parse(error.requestResult.requestRaw);
+	}
+	if (error.requestResult && error.requestResult.responseRaw) {
+		error.requestResult.responseRaw = JSON.parse(error.requestResult.responseRaw);
+	}
+
+	return JSON.stringify(error, null, 2);
+}
+*/
+
 export async function wait(delay: number): Promise<void> {
 	return new Promise(resolve => setTimeout(resolve, delay));
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,39 +1,50 @@
 import { Readable } from 'stream';
-import { secret, wait, isTypeInSchema } from './helpers';
+import { secret, /*client,*/ wait, isTypeInSchema } from './helpers';
 
+// import { FaunaQueryResult } from '../src/types';
 import { uploadSchema } from '../src/lib/schema';
+// import { uploadData } from '../src/lib/data';
+// import { uploadResources } from '../src/lib/resources';
 
 const timestamp = Date.now();
 
 // Wait a little bit between each test to avoid flooding the database service
-afterEach(async () => await wait(500));
+beforeEach(() => wait(500));
 
 test("01. override GraphQL schema", async() => { // {{{
 
-	const schema = Readable.from(`type MySuperAwesomeNewType_${timestamp} { field: Boolean }`);
+	const schema = Readable.from(`type TestType${timestamp} { field: Boolean }`);
 	await expect(uploadSchema(schema, secret, true)).resolves.toBe(true);
 
 	await wait(500);
-	await expect(isTypeInSchema(`MySuperAwesomeNewType_${timestamp}`)).resolves.toBe(true);
+	await expect(isTypeInSchema(`TestType${timestamp}`)).resolves.toBe(true);
+	await expect(isTypeInSchema(/^User/)).resolves.toBe(false);
 
-}, 90*1000 + 500 + 2000); // }}}
+}, 3000 + 90*1000 + 500); // }}}
 
 test("02. update GraphQL schema", async () => { // {{{
 
-	const schema = Readable.from(`type MySuperAwesomeNewType_${timestamp}_UPDATED { field: Boolean }`);
+	const schema = Readable.from(`type User${timestamp} { key: String! @unique(index: "user_keys") }`);
 	await expect(uploadSchema(schema, secret, false)).resolves.toBe(true);
 
 	await wait(500);
-	await expect(isTypeInSchema(`MySuperAwesomeNewType_${timestamp}`)).resolves.toBe(true);
-	await expect(isTypeInSchema(`MySuperAwesomeNewType_${timestamp}_UPDATED`)).resolves.toBe(true);
+	await expect(isTypeInSchema(`User${timestamp}`)).resolves.toBe(true);
+	await expect(isTypeInSchema(/^TestType/)).resolves.toBe(true);
 
-}, 500 + 2000); // }}}
+}, 3000 + 500); // }}}
 
-// test("03. upload new data", () => { // {{{
-// }); // }}}
+/*
+test("03. upload new data", async () => { // {{{
 
-// test("04. update existing data", () => { // {{{
-// }); // }}}
+	await expect(uploadData(client, [{
+		collection: `User${timestamp}`,
+		index:      'user_keys',
+		key:        'key',
+		documents:  [{ key: '' + timestamp }],
+	}])).resolves.toBe(FaunaQueryResult.Created);
 
-// test("05. upload functions", () => { // {{{
-// }); // }}}
+	// await wait(500);
+	// await expect(getDocument(client, 'my_data', 'my_index', `document_${timestamp}`)).resolves.toBeTruthy;
+
+}, 3000); // }}}
+*/
